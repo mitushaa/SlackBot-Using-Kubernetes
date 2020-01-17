3bcd6b843f89eb54b0a5682f594804092263eb26
Mitusha arya <aryamitusha@gmail.com>
2020-01-17 18:20:47 +0530

    Update

--- bot.js
+++ bot.js
@@ -1,17 +1,30 @@
-
-
 var Botkit = require('botkit')
+var fs = require('fs') // NEW: Add this require (for loading from files).
 
 var controller = Botkit.slackbot({debug: false})
-controller
-  .spawn({
-    token: 'xoxb-912766940295-898135829698-ReK23aA0uzYExptawOUDTl6X' // Edit this line!
-  })
-  .startRTM(function (err) {
-    if (err) {
-      throw new Error(err)
-    }
-  })
+
+// START: Load Slack token from file.
+if (!process.env.slack_token_path) {
+  console.log('Error: Specify slack_token_path in environment')
+  process.exit(1)
+}
+
+fs.readFile(process.env.slack_token_path, function (err, data) {
+  if (err) {
+    console.log('Error: Specify token in slack_token_path file')
+    process.exit(1)
+  }
+  data = String(data)
+  data = data.replace(/\s/g, '')
+  controller
+    .spawn({token: data})
+    .startRTM(function (err) {
+      if (err) {
+        throw new Error(err)
+      }
+    })
+})
+// END: Load Slack token from file.
 
 controller.hears(
   ['hello', 'hi'], ['direct_message', 'direct_mention', 'mention'],
